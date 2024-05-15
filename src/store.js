40cb4a77b7e756fbb07e550d63736046b2c66a00
PRoy <sam469roger@gmail.com>
2024-05-15 17:09:27 +0530

    created a redux store

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -15,7 +17,12 @@ function reducer(state = initialState, action) {
     case 'account/requestLoan':
       if (state.loan > 0) return state;
       //TODO later
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
 
     case 'account/payLoan':
       return {
@@ -29,3 +36,20 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+store.dispatch({ type: 'account/deposit', payload: 500 });
+console.log(store.getState());
+
+store.dispatch({ type: 'account/withdraw', payload: 200 });
+console.log(store.getState());
+
+store.dispatch({
+  type: 'account/requestLoan',
+  payload: { amount: 1000, purpose: 'buy a car' },
+});
+console.log(store.getState());
+
+store.dispatch({ type: 'account/payLoan' });
+console.log(store.getState());
