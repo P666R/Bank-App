40adab1260f19656400f56658dfb4581f066be3a
PRoy <sam469roger@gmail.com>
2024-05-17 13:08:32 +0530

    redux-logger for understanding redux flow

--- src/store.js
+++ src/store.js
@@ -1,13 +1,17 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, compose, combineReducers, createStore } from 'redux';
+import { logger } from 'redux-logger/src';
 
 import accountReducer from './features/accounts/accountSlice';
 import customerReducer from './features/customers/customerSlice';
+
+const middleware = [logger];
+const composedEnhancers = compose(applyMiddleware(...middleware));
 
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, undefined, composedEnhancers);
 
 export default store;
