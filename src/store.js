a89bc79a9bef64a4448b5cd9655ebd96860db72b
PRoy <sam469roger@gmail.com>
2024-05-18 16:48:36 +0530

    making api call with redux-thunk

--- src/store.js
+++ src/store.js
@@ -1,17 +1,17 @@
-import { applyMiddleware, compose, combineReducers, createStore } from 'redux';
-import { logger } from 'redux-logger/src';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { thunk } from 'redux-thunk';
+import { logger } from 'redux-logger';
 
 import accountReducer from './features/accounts/accountSlice';
 import customerReducer from './features/customers/customerSlice';
 
-const middleware = [logger];
-const composedEnhancers = compose(applyMiddleware(...middleware));
+const middleware = [thunk, logger];
 
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer, undefined, composedEnhancers);
+const store = createStore(rootReducer, applyMiddleware(...middleware));
 
 export default store;
